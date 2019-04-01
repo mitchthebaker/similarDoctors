8457aaf4473ba7f22f99e53317cc998a99b652fd
Mitchel Baker <mitchrpbaker@gmail.com>
2019-04-01 03:14:43 +0200

    Updated README.md, added final documentation to .js files

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 A web application aimed at users searching for the perfect doctor to come into contact with. 
 
-### Check out the site live:
+### Check out the site:
 
-Heroku link: 
+Heroku link: http://similardocs.herokuapp.com/
 
 #### Assumptions:
-1. I made the assumption that all of the data for the doctors had already been recorded. I potential feature for the future would be adding new doctors, along with their respective backgrounds (ie. Specialty, area, score, etc.)
+1. I made the assumption that all of the data for the doctors had already been recorded. 
 2. Additionally, I have all of the doctor's data stored on a .js file. In reality, the doctor's data would be stored in a secure database and not on a .js that is easy to access.
+
+#### Total time spent: 
+1. Around an hour total was spent outlining the whole application prior to writing the code
+2. The total time for coding took around 5 hours, but was spread out throughout the weekend.
+
+#### Other changes:
+1. There are many more features that I'd be interested in implementing. To name a few examples: a search bar for looking up the names of doctors, a suggested page where users can find the most recommended doctor, user authorization/login, etc.
+2. Visual changes, styling improvements
+3. Small bug fixes 
--- public/scripts/ui/doctorClick.js
+++ public/scripts/ui/doctorClick.js
@@ -7,6 +7,11 @@ let data = doctorData;
 /* 'element' object literal will contain all the query's to DOM elements
  *
  *      'listDoctors' is the name given to the DOM <section> element of class name 'listDoctors'
+ *      'doctorDivs' is a list of all the DOM elements with class 'doctorDiv'
+ *      'doctorDivsArray()' is the same as 'doctorDivs' but as an Array
+ *      'overlay' is the <section> element with the corresponding class name
+ *      'overlay_inner' is the <div> within the 'overlay' <section> element
+ *      'closeOverlay' is the <button> element that closes the 'overlay element
  */
 let element = {
     listDoctors: document.querySelector('.listDoctors'),
@@ -19,21 +24,36 @@ let element = {
     closeOverlay: document.querySelector('.closeOverlay')
 };
 
+//The openEvent() function adds the showOverlay class for
+// each one of the 'doctorDiv' elements when clicked
 const openEvent = function() {
     element.doctorDivs.forEach(() => element.overlay.classList.add('showOverlay'));
 };
 
+/* The updateDoctorInfo() function uses the 'doctorDivsArray' Array,
+ * and then applies the .forEach() method to all of it's contents.
+ */
 const updateDoctorInfo = function() {
+
     element.doctorDivsArray().forEach(function(el) {
+
+        //An .addEventListener() method is applied to the 'el' element clicked
         el.addEventListener('click', function() {
+
+            //The while loop removes any elements that were in the 'overlay_inner'
+            //tag previously
             while(element.overlay_inner.firstChild) {
                 element.overlay_inner.removeChild(element.overlay_inner.firstChild);
             }
 
-            let doctor = data[element.doctorDivsArray().indexOf(el)]
+            //Variable 'doctor' is the 'doctorDiv' tag that the user clicked on
+            let doctor = data[element.doctorDivsArray().indexOf(el)];
+
+            //Variable 'doctorDiv' is a new <div> element
             let doctorDiv = document.createElement('div');
 
             //Create the inner element for displaying related doctors
+            //Array relatedDoctors holds the doctor's that have a score of 5
             let relatedDoctors = [];
             data.forEach(function(element) {
                 if(element.score === 5) {
@@ -41,6 +61,7 @@ const updateDoctorInfo = function() {
                 }
             });
 
+            //The elements below will be added to the inside of the new 'doctorDiv' element
             doctorDiv.innerHTML = "<div class='currentDoctor'>" +
                                     "<h3 class='doctorDiv-innerh3'>" + doctor.name + "</h3>" +
                                     "<h4 class='doctorDiv-innerh4'>" + "Profession: " + doctor.profession + "</h4>" +
@@ -58,15 +79,21 @@ const updateDoctorInfo = function() {
                                     "<h3 class='doctorDiv-innerh3'>" + relatedDoctors[3].name + "</h3>" +
                                     "<h4 class='doctorDiv-innerh4'>" + "Score: " + relatedDoctors[3].score + "</h4>" +
                                   "</div>";
+
+            //'doctorDiv' is given the class name 'doctorDiv-overlay'
             doctorDiv.setAttribute('class', 'doctorDiv-overlay');
+
+            //'doctorDiv' is appended within the 'overlay_inner' tag
             element.overlay_inner.appendChild(doctorDiv);
         });
     });
 }
 
+//.addEventListener() methods that correspond to the functions above
 element.listDoctors.addEventListener('click', openEvent, false);
 element.listDoctors.addEventListener('click', updateDoctorInfo, false);
 
+//When the user clicks the 'closeOverlay' <button> tag, the overlay will close
 element.closeOverlay.addEventListener('click', function() {
     element.overlay.classList.remove('showOverlay');
 }, false);
